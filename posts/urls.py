362fd1b9654a516698544aa706eeed97df616eb6
Temple2001 <kimarang0@naver.com>
2024-05-06 23:28:18 +0900

    FEAT: Submit Week7 Challenge Assignment - post api 재구성

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ from posts.views import *
 urlpatterns = [
     path('introduction', introduction_json, name='introduction_json'),
     path('page', page_dynamic, name='page_dynamic'),
-    path('<int:id>', get_post_detail, name='게시글 조회'),
+    path('', PostList.as_view(), name='get_posts_last_week'),
+    path('<int:id>', PostDetail.as_view(), name='get_post_by_id'),
     path('tagging', get_tag_relationship, name='get_tag_relationship'),
     path('<int:id>/comment', CommentList.as_view(), name='get_comment_all'),
-    path('posts-last-week', get_posts_last_week, name='get_posts_last_week'),
 ]
