8dda63a68c2d44545748aef9da52a2ca5f6faadc
Temple2001 <kimarang0@naver.com>
2024-06-08 23:58:19 +0900

    FEAT: Submit Week10 Standard Assignment

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
 from .models import Comment, Post
+from rest_framework.exceptions import ValidationError
+from PIL import Image
+import io
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = "__all__"
+
+    def validate_thumbnail(self, value):
+        if value and value.content_type == 'image/png':
+            raise ValidationError("PNG 형식의 이미지는 업로드할 수 없습니다.")
+        elif value:
+            img = Image.open(value)
+            width, height = img.size
+            img_small = img.resize((width//2, height//2))
+            img_io = io.BytesIO()
+            img_small.save(img_io, format=img.format)
+            value.file = img_io
+        return value
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
